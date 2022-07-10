afd37ecc85d59a0903cab4a838ef4845ffa4bef5
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-07-10 23:21:08 +0300

    completing the design

--- app/src/main/java/com/example/nutritionsapp/data/DataManager.kt
+++ app/src/main/java/com/example/nutritionsapp/data/DataManager.kt
@@ -5,7 +5,63 @@ import com.example.nutritionsapp.data.domain.Meal
 class DataManager {
     private val mealsList = mutableListOf<Meal>()
 
-    fun addMeal(meal: Meal){
+    private val addedItems = mutableListOf(
+        Meal(
+            calories = "200",
+            name = "Pizza",
+            caffeine = "10",
+            fat = "10",
+            protein = "10",
+            saturatedFat = "10",
+            servingSize = "100",
+            totalFat = "10",
+        ),
+        Meal(
+            calories = "150",
+            name = "Burger",
+            caffeine = "10",
+            fat = "10",
+            protein = "10",
+            saturatedFat = "10",
+            servingSize = "100",
+            totalFat = "10",
+        ),
+        Meal(
+            calories = "100",
+            name = "Chips",
+            caffeine = "10",
+            fat = "10",
+            protein = "10",
+            saturatedFat = "10",
+            servingSize = "100",
+            totalFat = "10",
+        ),
+        Meal(
+            calories = "50",
+            name = "Potatoes",
+            caffeine = "10",
+            fat = "10",
+            protein = "10",
+            saturatedFat = "10",
+            servingSize = "100",
+            totalFat = "10",
+        ),
+        Meal(
+            calories = "50",
+            name = "Tomatoes",
+            caffeine = "10",
+            fat = "10",
+            protein = "10",
+            saturatedFat = "10",
+            servingSize = "100",
+            totalFat = "10",
+        )
+    )
+
+
+    fun getAllAddedMeals(): MutableList<Meal> = addedItems
+
+    fun addMeal(meal: Meal) {
         mealsList.add(meal)
     }
 
--- app/src/main/java/com/example/nutritionsapp/ui/CalorieFragment.kt
+++ app/src/main/java/com/example/nutritionsapp/ui/CalorieFragment.kt
@@ -1,18 +1,19 @@
 package com.example.nutritionsapp.ui
 
+import android.widget.ArrayAdapter
+import com.example.nutritionsapp.R
+import com.example.nutritionsapp.data.DataManager
+import com.example.nutritionsapp.data.domain.Meal
 import com.example.nutritionsapp.databinding.FragmentCalorieBinding
 import com.github.mikephil.charting.charts.PieChart
 
 
 class CalorieFragment : BaseFragment<FragmentCalorieBinding>(FragmentCalorieBinding::inflate) {
     override var LOG_TAG = "CalorieFragment"
+    private val addedItems: List<Meal> = DataManager().getAllAddedMeals()
 
-    override fun onStart() {
-        super.onStart()
-    }
 
     override fun addCallBacks() {
-
     }
 
 
