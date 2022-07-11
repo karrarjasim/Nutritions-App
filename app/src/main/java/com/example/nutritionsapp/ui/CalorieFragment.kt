b73f5a77b1a7c7dba97d87df74d64cf7987c7263
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-07-11 12:49:12 +0300

    merge develop into feature branch and change edits in desin

--- app/src/main/java/com/example/nutritionsapp/ui/CalorieFragment.kt
+++ app/src/main/java/com/example/nutritionsapp/ui/CalorieFragment.kt
@@ -1,15 +1,20 @@
 package com.example.nutritionsapp.ui
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.nutritionsapp.R
 import com.example.nutritionsapp.data.DataManager
 import com.example.nutritionsapp.data.domain.Meal
+import com.example.nutritionsapp.databinding.FragmentCalculateBinding
 import com.example.nutritionsapp.databinding.FragmentCalorieBinding
-import com.github.mikephil.charting.charts.PieChart
 
 
-class CalorieFragment : BaseFragment<FragmentCalorieBinding>(FragmentCalorieBinding::inflate) {
+class CalorieFragment : BaseFragment<FragmentCalorieBinding>() {
     override var LOG_TAG = "CalorieFragment"
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentCalorieBinding
+        get() = FragmentCalorieBinding::inflate
+
     private val addedItems: List<Meal> = DataManager().getAllAddedMeals()
 
 
