1172b078a6aec3ac7cff449a21a0295d097b33cb
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-07-14 15:31:41 +0300

    completing the splash screen

--- app/src/main/java/com/example/nutritionsapp/ui/CalculateActivity.kt
+++ app/src/main/java/com/example/nutritionsapp/ui/CalculateActivity.kt
@@ -2,11 +2,9 @@ package com.example.nutritionsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.nutritionsapp.R
 import com.example.nutritionsapp.databinding.ActivityCalculateBinding
-import com.example.nutritionsapp.databinding.ActivityHomeBinding
 
 class CalculateActivity : AppCompatActivity() {
 
@@ -20,7 +18,7 @@ class CalculateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculateBinding.inflate(layoutInflater)
-        installSplashScreen()
+        setTheme(R.style.splashScreenTheme)
 
         setContentView(binding.root)
 //        initSubView()
